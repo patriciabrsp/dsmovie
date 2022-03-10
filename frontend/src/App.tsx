61e02218614561e8b20c95f74675021e50005416
patriciabrsp <patriciabrsp@gmail.com>
2022-03-09 21:28:42 -0300

    routes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'Pages/Listing';
+import Form from 'Pages/Form';
+import Navbar from "components/Navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
